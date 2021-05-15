3682a8947f54b612181159346f337b8d32e13606
Naru-hub <1208ny0324@gmail.com>
2021-05-15 09:51:52 +0000

    add recipe_show

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '06c9a84c368f08f7d19f3afd328c3db3e3fd6a1912b1d530df8021225316598b438f535adeae2bb14caea66544e2ebb4c441ea8137ec93799855e3238921699a'
