71f75eb569d43154cbbbde338c28eb1ae30bcb4c
Naru-hub <1208ny0324@gmail.com>
2021-05-17 01:24:10 +0000

    add before_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
-  
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    
+
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: '不正なアクセスです。'
+    end
   end
 
   def update
